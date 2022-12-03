bfbd6468c3d805381b266688c851d9ab54fa2d66
kavinvin <kavinvin.vin@gmail.com>
2022-12-03 23:52:42 +0700

    Fix pipeline

--- 2022/haskell/src/Day1.hs
+++ 2022/haskell/src/Day1.hs
@@ -3,6 +3,7 @@ module Day1 (solution) where
 import Data.List.Split (splitOn)
 
 -- | Day 1: Calorie Counting
+-- | https://adventofcode.com/2022/day/1
 -- >>> solution "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
 -- "24000"
 solution :: String -> String
