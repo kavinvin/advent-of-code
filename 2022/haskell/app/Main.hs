388afce599039f3a7fc6e48e221dae243505db6c
kavinvin <kavinvin.vin@gmail.com>
2022-12-04 11:35:03 +0700

    Finish day 1 part 2

--- 2022/haskell/app/Main.hs
+++ 2022/haskell/app/Main.hs
@@ -1,10 +1,30 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main (main) where
 
+import Data.Typeable (Typeable)
+import Data.Data (Data)
+import System.Console.CmdArgs
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Data.Map as M
+
+data SolutionArgs = SolutionArgs { day :: Int, part :: Int } deriving (Show, Data, Typeable)
+
+solutionArgs = SolutionArgs { day = def &= help "Day of Advent of Code", part = def }
+         &= summary "Advent of Code solutions"
+
+solution :: SolutionArgs -> (String -> String)
+solution (SolutionArgs day part) = solutionMap M.! (day, part)
+  where solutionMap = M.fromList [ ((1, 1), Day1.solution)
+                                 , ((1, 2), Day1.solution2)
+                                 , ((2, 1), Day2.solution)
+                                 , ((3, 1), Day3.solution) ]
 
 -- Receive input from stdin
--- Tips for macOS: pbpaste | stack run
+-- Tips for macOS: pbpaste | stack run -- --day 1
 main :: IO ()
-main = interact Day3.solution
+-- main = interact Day3.solution
+main = do
+  parsedArgs <- cmdArgs solutionArgs
+  interact (solution parsedArgs)
--- 2022/haskell/src/Day1.hs
+++ 2022/haskell/src/Day1.hs
@@ -1,13 +1,23 @@
-module Day1 (solution) where
+module Day1 (solution, solution2) where
 
+import Data.List (sort)
 import Data.List.Split (splitOn)
 
 -- | Day 1: Calorie Counting
 -- | https://adventofcode.com/2022/day/1
 -- >>> solution "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
 -- "24000"
+-- >>> solution2 "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+-- "45000"
 solution :: String -> String
 solution input = show maxCalories
   where maxCalories = maximum . fmap caloriesPerElf . fmap lines . splitOn elfSeparator $ input
         caloriesPerElf = sum . fmap read
         elfSeparator = "\n\n"
+
+solution2 :: String -> String
+solution2 input = show max3Calories
+  where max3Calories = sumTop3 . fmap caloriesPerElf . fmap lines . splitOn elfSeparator $ input
+        caloriesPerElf = sum . fmap read
+        elfSeparator = "\n\n"
+        sumTop3 = sum . take 3 . reverse . sort
