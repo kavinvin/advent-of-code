85a4d1f329c8fcb3f40484232b75907d1b06b5b1
kavinvin <kavinvin.vin@gmail.com>
2022-12-04 12:27:22 +0700

    Update

--- 2022/haskell/src/Day4Part1.hs
+++ 2022/haskell/src/Day4Part1.hs
@@ -5,6 +5,8 @@ import Data.List (isSubsequenceOf, length)
 
 count f xs = length $ filter f xs
 
+withinEachOther xs ys = xs `isSubsequenceOf` ys || ys `isSubsequenceOf` xs
+
 -- |
 -- >>> solution "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
 -- "2"
@@ -13,4 +15,3 @@ solution = show . count (== True) . fmap compute . lines
   where splitLine line = let [first, second] = splitOn "," line in (toRange first, toRange second)
         toRange word = let [from, to] = splitOn "-" word in [(read from :: Int)..(read to :: Int)]
         compute line = let (first, second) = splitLine line in first `withinEachOther` second
-        withinEachOther xs ys = xs `isSubsequenceOf` ys || ys `isSubsequenceOf` xs
--- 2022/haskell/src/Day4Part2.hs
+++ 2022/haskell/src/Day4Part2.hs
@@ -3,7 +3,9 @@ module Day4Part2 (solution) where
 import Data.List.Split (splitOn)
 import Data.List (isSubsequenceOf, length, intersect)
 
-count f xs = length $ filter f xs
+count f = length . filter f
+
+overlap xs ys = (length $ xs `intersect` ys) > 0
 
 -- |
 -- >>> solution "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
@@ -13,4 +15,3 @@ solution = show . count (== True) . fmap compute . lines
   where splitLine line = let [first, second] = splitOn "," line in (toRange first, toRange second)
         toRange word = let [from, to] = splitOn "-" word in [(read from :: Int)..(read to :: Int)]
         compute line = let (first, second) = splitLine line in first `overlap` second
-        overlap xs ys = (length $ xs `intersect` ys) > 0
